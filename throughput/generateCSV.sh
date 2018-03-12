1b82ed57d4abb1e786ab20cd827fc3288d4a111d
ChrisHan <chris@ChrisHans-MacBook-Pro.local>
2018-03-11 23:05:54 -0700

    Add Throughput

---
+++ throughput/generateCSV.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in 1 2 4 8 # number of threads
+	do
+		for j in 800 200 50 12 3 # number fo size
+			do
+				sudo toplev.py -l3 -I 1000 -x, -o plot/CSV$i$j.csv perf stat -d java -jar MultithreadJackson.jar $i 1 $j input.txt
+			done
+	done
+
+exit 0
+
+
