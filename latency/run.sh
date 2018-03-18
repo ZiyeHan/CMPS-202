fd5dbede9ca2cbc51250ccb333be81f159af8f15
ChrisHan <chris@ChrisHans-MacBook-Pro.local>
2018-03-18 02:06:35 -0700

    Upload some materials

--- latency/run.sh
+++ latency/run.sh
@@ -3,7 +3,7 @@
 docker run -dit --privileged --name cmpe202-vanilla-music-latency-container chrishan82/cmpe202-vanilla-music-latency
 
 docker exec cmpe202-vanilla-music-latency-container /bin/sh -c "wget https://raw.githubusercontent.com/ZiyeHan/CMPS-202/master/latency/test.sh"
-docker exec cmpe202-vanilla-music-latency-container test.sh
+docker exec cmpe202-vanilla-music-latency-container /bin/sh -c "bash test.sh"
 
 
 
--- latency/test.sh
+++ latency/test.sh
@@ -1,3 +1,50 @@
-echo "1s"
+echo "important !!!"
+echo "Now you need to manualy connect your android divice to the machine. You have 15 seconds."
+sleep 15s
+
+# install the apk to your device
+adb install VanillaMusic.apk
 sleep 5s
-echo "6s"
+# start systrace monitor for 200*500/1000=100 seconds, monitor latency like frames
+cd ./android-sdk-linux/platform-tools/systrace
+
+echo "Wait for First Run..."
+python systrace.py --time=50 -o latencyResult1.html
+
+# start monkey runner for hand-free aumotation testing
+# send 200 random actions to the app, interval is 500ms between events
+adb shell monkey -p ch.blinkenlights.android.vanilla -v --throttle 300 500
+# Cool! Monkeyrunner start working!
+
+echo "Wait for Second Run..."
+sleep 10s
+python systrace.py --time=50 -o latencyResult2.html
+
+# start monkey runner for hand-free aumotation testing
+# send 200 random actions to the app, interval is 500ms between events
+adb shell monkey -p ch.blinkenlights.android.vanilla -v --throttle 300 500
+# Cool! Monkeyrunner start working!
+
+echo "Wait for Third Run..."
+sleep 10s
+
+python systrace.py --time=50 -o latencyResult3.html
+
+# start monkey runner for hand-free aumotation testing
+# send 200 random actions to the app, interval is 500ms between events
+adb shell monkey -p ch.blinkenlights.android.vanilla -v --throttle 300 500
+# Cool! Monkeyrunner start working!
+
+echo "Wait for Fourth Run..."
+sleep 10s
+python systrace.py --time=50 -o latencyResult4.html
+
+# start monkey runner for hand-free aumotation testing
+# send 200 random actions to the app, interval is 500ms between events
+adb shell monkey -p ch.blinkenlights.android.vanilla -v --throttle 300 500
+# Cool! Monkeyrunner start working!
+
+# collect systrace visualization latency report in HTML web page
+
+
+
