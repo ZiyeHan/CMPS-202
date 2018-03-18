260f990ce7ba5ae11dca765374dea3ee854e3207
ChrisHan <chris@ChrisHans-MacBook-Pro.local>
2018-03-18 02:36:36 -0700

    Upload some materials

--- latency/test.sh
+++ latency/test.sh
@@ -1,6 +1,6 @@
 echo "important !!!"
-echo "Now you need to manualy connect your android divice to the machine. You have 25 seconds."
-sleep 25s
+echo "Now you need to manualy connect your android divice to the machine. You have 30 seconds."
+sleep 30s
 
 # install the apk to your device
 adb install VanillaMusic.apk
@@ -9,40 +9,39 @@ sleep 5s
 cd ./android-sdk-linux/platform-tools/systrace
 
 echo "Wait for First Run..."
-python systrace.py --time=50 -o latencyResult1.html
-
 # start monkey runner for hand-free aumotation testing
 # send 200 random actions to the app, interval is 500ms between events
 adb shell monkey -p ch.blinkenlights.android.vanilla -v --throttle 300 500
 # Cool! Monkeyrunner start working!
+python systrace.py --time=50 -o latencyResult1.html
+
 
 echo "Wait for Second Run..."
 sleep 10s
-python systrace.py --time=50 -o latencyResult2.html
-
 # start monkey runner for hand-free aumotation testing
 # send 200 random actions to the app, interval is 500ms between events
 adb shell monkey -p ch.blinkenlights.android.vanilla -v --throttle 300 500
 # Cool! Monkeyrunner start working!
+python systrace.py --time=50 -o latencyResult2.html
+
 
 echo "Wait for Third Run..."
 sleep 10s
-
-python systrace.py --time=50 -o latencyResult3.html
-
 # start monkey runner for hand-free aumotation testing
 # send 200 random actions to the app, interval is 500ms between events
 adb shell monkey -p ch.blinkenlights.android.vanilla -v --throttle 300 500
 # Cool! Monkeyrunner start working!
+python systrace.py --time=50 -o latencyResult3.html
+
 
 echo "Wait for Fourth Run..."
 sleep 10s
-python systrace.py --time=50 -o latencyResult4.html
-
 # start monkey runner for hand-free aumotation testing
 # send 200 random actions to the app, interval is 500ms between events
 adb shell monkey -p ch.blinkenlights.android.vanilla -v --throttle 300 500
 # Cool! Monkeyrunner start working!
+python systrace.py --time=50 -o latencyResult4.html
+
 
 # collect systrace visualization latency report in HTML web page
 
