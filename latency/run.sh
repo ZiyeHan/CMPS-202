eaf17dd75468777fb42511b2c482c2c32c160ab8
ChrisHan <chris@ChrisHans-MacBook-Pro.local>
2018-03-18 01:55:41 -0700

    Upload some materials

--- latency/run.sh
+++ latency/run.sh
@@ -2,7 +2,10 @@
 
 docker run -dit --privileged --name cmpe202-vanilla-music-latency-container chrishan82/cmpe202-vanilla-music-latency
 
-docker exec cmpe202-vanilla-music-latency-container /bin/sh -c "wget "
+docker exec cmpe202-vanilla-music-latency-container /bin/sh -c "wget https://raw.githubusercontent.com/ZiyeHan/CMPS-202/master/latency/test.sh"
+docker exec cmpe202-vanilla-music-latency-container test.sh
+
+
 
 
 
--- latency/test.sh
+++ latency/test.sh
@@ -1,7 +1,3 @@
-echo -n "Is this a good question (y/n)? "
-read answer
-if echo "$answer" | grep -iq "^y" ;then
-    echo Yes
-else
-    echo No
-fi
+echo "1s"
+sleep 5s
+echo "6s"
