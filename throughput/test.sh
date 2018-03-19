077ed19a30f8d4bec28ea4816c0eabef6d018d81
ChrisHan <chris@ChrisHans-MacBook-Pro.local>
2018-03-19 02:04:58 -0700

    Upload some materials

--- throughput/test.sh
+++ throughput/test.sh
@@ -40,7 +40,7 @@ for i in 1 2 4 8 # number of threads
 	do
 		for j in 800 200 50 12 3 # number fo size
 			do
-				perf stat -d -o ./result$i$j MultithreadJackson.jar $i 1 $j input.txt
+				perf stat -d -o ./result$i$j java -jar MultithreadJackson.jar $i 1 $j input.txt
 			done
 	done
 
