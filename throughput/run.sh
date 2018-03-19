eec0ca019882fda14c10453f51fd24edc6e8ff10
ChrisHan <chris@ChrisHans-MacBook-Pro.local>
2018-03-18 21:33:23 -0700

    Upload some materials

--- throughput/run.sh
+++ throughput/run.sh
@@ -2,5 +2,5 @@
 
 docker run -dit --privileged --name cmpe202-jackson-throughput-container chrishan82/cmpe202-jackson-throughput
 
-docker exec cmpe202-vanilla-music-latency-container /bin/sh -c "wget https://raw.githubusercontent.com/ZiyeHan/CMPS-202/master/throughput/test.sh"
-docker exec cmpe202-vanilla-music-latency-container /bin/sh -c "bash test.sh"
+docker exec cmpe202-jackson-throughput-container  /bin/sh -c "wget https://raw.githubusercontent.com/ZiyeHan/CMPS-202/master/throughput/test.sh"
+docker exec cmpe202-jackson-throughput-container  /bin/sh -c "bash test.sh"
--- throughput/test.sh
+++ throughput/test.sh
@@ -4,7 +4,7 @@ mkdir PlotFiles
 mkdir ImageFiles
 
 # close nmi_watchdog
-echo 0 | sudo dd of="/proc/sys/kernel/nmi_watchdog"
+echo 0 | dd of="/proc/sys/kernel/nmi_watchdog"
 
 
 
