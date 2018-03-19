eaad7fbc469b09a12b7a0ecfe72f8d4fc9314f2e
ChrisHan <chris@ChrisHans-MacBook-Pro.local>
2018-03-18 22:22:29 -0700

    Upload some materials

--- throughput/test.sh
+++ throughput/test.sh
@@ -6,12 +6,14 @@ mkdir ImageFiles
 # close nmi_watchdog
 echo 0 | dd of="/proc/sys/kernel/nmi_watchdog"
 
+apt-get install -y python3-tk
+
 # generate csv
 for i in 1 2 4 8 # number of threads
 	do
 		for j in 800 200 50 12 3 # number fo size
 			do
-				python pmu-tools/toplev.py -l3 -I 1000 -x, -o PlotFiles/CSV$i$j.csv perf stat -d java -jar MultithreadJackson.jar $i 1 $j input.txt
+				python3 pmu-tools/toplev.py -l3 -I 1000 -x, -o PlotFiles/CSV$i$j.csv perf stat -d java -jar MultithreadJackson.jar $i 1 $j input.txt
 			done
 	done
 
@@ -21,7 +23,7 @@ for i in 1 2 4 8 # number of threads
 	do
         for j in 800 200 50 12 3 # number fo size 
         	do
-        		python pmu-tools/tl-barplot.py --cpu C0-T0 PlotFiles/CSV$i$j.csv -o ImageFiles/image$i$j.png
+        		python3 pmu-tools/tl-barplot.py --cpu C0-T0 PlotFiles/CSV$i$j.csv -o ImageFiles/image$i$j.png
         	done
 	done
 
